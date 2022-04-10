1578e5bc4da7a1a83ea5ba112034b0fc0ed8caaa
Maximilian Krause <max@kuatsu.dev>
2022-04-10 11:58:23 +0200

    feat: added Flipper support

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as Localization from 'expo-localization';
 import I18n from 'i18n-js';
+import { connectToDevTools } from 'react-devtools-core';
 import Locales from './config/locales';
 import StackNavigator from './navigation';
 
@@ -8,6 +9,13 @@ I18n.translations = Locales;
 I18n.defaultLocale = 'en';
 I18n.locale = Localization.locale;
 I18n.fallbacks = true;
+
+if (__DEV__) {
+  connectToDevTools({
+    host: 'localhost',
+    port: 8097,
+  });
+}
 
 const App = () => (
   <StackNavigator />
