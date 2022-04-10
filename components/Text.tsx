ee5a33af63338e1eef4c422caf3f80320b97a3d6
Maximilian Krause <max@kuatsu.dev>
2022-04-10 12:46:53 +0200

    fix: fixed some stuff in Text component

--- components/Text.tsx
+++ components/Text.tsx
@@ -7,16 +7,12 @@ import {
 
 const Text = (props: TextProps) => {
   const { children, style } = props;
-  const styles = style;
-  if (style) {
-    const objStyle = StyleSheet.flatten(style);
-    if (!objStyle.fontFamily) {
-      /* You can optionally define a default font family and additional font weight families here
-         (don't forget to change styles from const to let, too!) */
-      /* objStyle.fontFamily = 'Outfit_500Medium';
-      if (objStyle.fontWeight === 'bold' || objStyle.fontWeight === '700') objStyle.fontFamily = 'Outfit_700Bold';
-      styles = objStyle; */
-    }
+  let styles = style;
+  const objStyle = StyleSheet.flatten(style || {});
+  if (!objStyle.fontFamily || objStyle.fontFamily === 'Outfit') {
+    objStyle.fontFamily = 'Outfit_500Medium';
+    if (objStyle.fontWeight === 'bold' || objStyle.fontWeight === '700') objStyle.fontFamily = 'Outfit_700Bold';
+    styles = objStyle;
   }
   return (
     <RNText {...props} style={styles}>
