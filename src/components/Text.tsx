ef333551716d01b06deed90006e708c9dbeb7d4a
Maximilian Krause <max@kuatsu.dev>
2022-04-11 23:50:15 +0200

    fix: fixed custom Text component style applying
    When passed an object in the style prop, the font family wasn't applied
    correctly (only when passed as an array of style objects).

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -3,12 +3,15 @@ import {
   TextProps,
   Text as RNText, // eslint-disable-line no-restricted-imports
   StyleSheet,
+  StyleProp,
 } from 'react-native';
 
 const Text = (props: TextProps) => {
   const { children, style } = props;
   let styles = style;
-  const objStyle = StyleSheet.flatten(style || {});
+  let objStyle: StyleProp<any> = {};
+  if (Array.isArray(style)) objStyle = StyleSheet.flatten(style);
+  else if (style) objStyle = { ...style as object };
   if (!objStyle.fontFamily || objStyle.fontFamily === 'Outfit') {
     objStyle.fontFamily = 'Outfit_500Medium';
     if (objStyle.fontWeight === 'bold' || objStyle.fontWeight === '700') objStyle.fontFamily = 'Outfit_700Bold';
